967293d3e45d824eb9d813979f721e634e9154b1
Kari L <kjlarson1@outlook.com>
2019-05-27 18:13:00 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Toggle navigation
+$(document).ready(function() {
+  
+  $("#toggle").click(function() {
+    $(this).toggleClass("on");
+    $("#resize").toggleClass("active");
+  });
+});
+
+// Back to top arrow
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
